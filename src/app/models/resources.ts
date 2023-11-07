284455e1941f3d4f88b31e0b319097165860780c
Kooper Smith <kooper.smith@leadingedje.com>
2023-11-07 13:07:09 -0500

    make resources readonly

--- src/app/models/resources.ts
+++ src/app/models/resources.ts
@@ -1,18 +1,18 @@
 export default class Resources {
 
-  static reticle = {
+  static readonly reticle = {
     gltf: "https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf"
   };
 
-  static flower = {
+  static readonly flower = {
     gltf: "https://immersive-web.github.io/webxr-samples/media/gltf/sunflower/sunflower.gltf"
   }
 
-  static fence = {
+  static readonly fence = {
     png: "https://png.pngtree.com/png-vector/20220805/ourmid/pngtree-picket-fence-ancient-architecture-arrowhead-png-image_5750495.png"
   };
 
-  static grid = {
+  static readonly grid = {
     png: "/assets/textures/texture_1m x 1m.png"
   };
 
